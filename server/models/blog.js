746b94aac21ce1eb80541ee7c61d7c645b546c04
Brendan Prinz <brendanprinz@gmail.com>
2018-01-02 11:24:04 -0800

    fixed tests updated mongoose

--- server/models/blog.js
+++ server/models/blog.js
@@ -6,10 +6,10 @@ const Schema = mongoose.Schema;
 const BlogSchema = new Schema({
     title: { type: String, required: true }, // firstName property is a string and required
     article: { type: String, required: true },
-    published: { type: Date, required: true },
-    featured: { type: Boolean, required: true },
+    published: { type: Date, required: false },
+    featured: { type: Boolean, required: false },
     author: { type: Schema.Types.ObjectId, ref: "User" } //how would you insert this into database?
     //author or authorId
-});
+}, { usePushEach: true});
 
 module.exports = mongoose.model('Blog', BlogSchema);
--- server/models/user.js
+++ server/models/user.js
@@ -14,6 +14,6 @@ const UserSchema = new Schema({
     },
     blogs: [{ type: Schema.Types.ObjectId, ref: 'Blog' }]
     
-});
+}, { usePushEach: true});
 
 module.exports = mongoose.model('User', UserSchema);
